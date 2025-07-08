5e92600da6e2bc16897a8ab2d08b9da12f9527b4
Fisherman August <fshrmn.august@gmail.com>
2025-07-08 20:35:33 +0300

    prac1 result

--- abs_test.go
+++ abs_test.go
@@ -1,1 +1,31 @@
 package main
+
+import "testing"
+
+func TestAbs(t *testing.T) {
+
+	tests := []struct { // добавляем слайс тестов
+		name  string
+		value float64
+		want  float64
+	}{
+		{
+			name:  "simple test #1", // описываем каждый тест:
+			value: -1,               // значения, которые будет принимать функция,
+			want:  1,                // и ожидаемый результат
+		},
+		{
+			name:  "zero",
+			value: -0,
+			want:  0,
+		},
+	}
+	for _, test := range tests { // цикл по всем тестам
+		t.Run(test.name,
+			func(t *testing.T) {
+				if got := Abs(test.value); got != test.want {
+					t.Errorf("Abs(%v) = %v, want %v", test.value, got, test.want)
+				}
+			})
+	}
+}
