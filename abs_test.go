4ae88e573ba41a3556593137def2331849370613
Fisherman August <fshrmn.august@gmail.com>
2025-07-08 20:53:04 +0300

    assert

--- abs_test.go
+++ abs_test.go
@@ -1,31 +1,16 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+	assert — содержит функции, которые проверяют выполнение условий, сравнивают числа, строки и более сложные объекты (JSON, YAML). Эти функции возвращают значение типа bool. Если проверяемое условие не выполнено, тест выдаёт ошибку, но продолжает свою работу.
+
+require — содержит тот же набор функций, но они не возвращают значения. Если проверка не пройдена, работа теста прекращается.
+*/
 func TestAbs(t *testing.T) {
-
-	tests := []struct { // добавляем слайс тестов
-		name  string
-		value float64
-		want  float64
-	}{
-		{
-			name:  "simple test #1", // описываем каждый тест:
-			value: -1,               // значения, которые будет принимать функция,
-			want:  1,                // и ожидаемый результат
-		},
-		{
-			name:  "zero",
-			value: -0,
-			want:  0,
-		},
-	}
-	for _, test := range tests { // цикл по всем тестам
-		t.Run(test.name,
-			func(t *testing.T) {
-				if got := Abs(test.value); got != test.want {
-					t.Errorf("Abs(%v) = %v, want %v", test.value, got, test.want)
-				}
-			})
-	}
+	assert.Equal(t, 8.0, Abs(-8))
 }
